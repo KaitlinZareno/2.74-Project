5533a8d2c46f5aa18fd85dc0332b151b593fbfa8
KaitlinZareno <kaitlinzareno@gmail.com>
2021-11-22 14:22:10 -0500

    static swing leg

--- derive_system_leg.m
+++ derive_system_leg.m
@@ -1,20 +1,20 @@
 name = 'leg';
 
 % Define variables for time, generalized coordinates + derivatives, controls, and parameters 
-syms x y t th1 th2 th3 dx dy dth1 dth2 dth3 ddx ddy ddth1 ddth2 ddth3 real
-syms m0 m1 m2 m3 m4 m5 I1 I2 I3 I4 I5 l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 g k real
+syms x y t th1 th2 th3 ths xs ys dx dy dth1 dth2 dth3 dths dxs dys ddx ddy ddth1 ddth2 ddth3 ddths ddxs ddys real
+syms m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_C_s ls g k real
 syms l_OA l_OB l_AC l_DE l_IG l_GH l_heela real 
-syms tau1 tau2 tau3 Fx Fy real
+syms tau1 tau2 tau3 taus Fx Fy real
 syms Ir N real
 
 % Group them
 q   = [x; y; th1 ; th2 ; th3; ths];      % generalized coordinates
-dq  = [dx; dy; dth1 ; dth2; dth3];    % first time derivatives
-ddq = [ddx; ddy; ddth1;ddth2; ddth3];  % second time derivatives
-u   = [tau1 ; tau2; tau3];     % controls
+dq  = [dx; dy; dth1 ; dth2; dth3 ; dths];    % first time derivatives
+ddq = [ddx; ddy; ddth1;ddth2; ddth3 ; ddths];  % second time derivatives
+u   = [tau1 ; tau2; tau3; taus];     % controls
 F   = [Fx ; Fy];
 
-p   = [m0 m1 m2 m3 m4 m5 I1 I2 I3 I4 I5 Ir N l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_OA l_OB l_AC l_DE l_IG l_GH l_heela g k]';        % parameters
+p   = [m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is Ir N l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_OA l_OB l_AC l_DE l_IG l_GH l_heela ls l_C_s g k]';        % parameters
 
 % Generate Vectors and Derivatives
 
@@ -26,6 +26,8 @@ khat = cross(ihat,jhat);
 th1hat = sin(th1)*ihat - cos(th1)*jhat;
 th2hat = sin(th2 + th1)*ihat - cos(th2 + th1)*jhat;
 th3hat = sin(th3 + th2 + th1)*ihat - cos(th3+ th2 + th1)*jhat;
+
+thshat = sin(ths)*ihat - cos(ths)*jhat;; %WANT SWING LEG TO BE THE MIRROR IMAGE OF TH1
 % ---------------------------------- %
 
 ddt = @(r) jacobian(r,[q;dq])*[dq;ddq]; % a handy anonymous function for taking time derivatives
@@ -52,6 +54,11 @@ rH = rE + l_GH*th3hat; %two havles of the same foot
 rI = rE-l_IG*th3hat; %second half MAKE NEGATIVE TO FLIP DIRECTION OF VECTOR, heel position
 rM5 = rE + c5*th3hat;
 
+%SWING LEG
+rMs = r0 + l_C_s*thshat;
+rHs = r0 + ls*thshat;
+
+%CALCULATE DERIVATIVES
 %velocity
 vR0 = ddt(r0);
 vM1 = ddt(rM1);
@@ -64,6 +71,12 @@ drE = ddt(rE);
 vM5 = ddt(rM5);
 drH = ddt(rH);
 drI = ddt(rI);
+
+%SWING LEG
+%velocity
+drHs = ddt(rHs);
+vS = ddt(rMs);
+
 
 % ---------------------------------- %
 
@@ -79,8 +92,12 @@ T2 = (1/2)*m2*dot(vM2', vM2)+ (1/2)*(I2)*dth2^2;
 T3 = (1/2)*m3*dot(vM3', vM3)+ (1/2)*(I3)*dth2^2;
 T4 = (1/2)*m4*dot(vM4', vM4)+ (1/2)*(I4)*dth1^2;
 
-%FOOT
 T5 = (1/2)*m5*dot(vM5', vM5)+ (1/2)*(I5)*dth3^2;
+
+
+%SWING LEG
+Ts = (1/2)*ms*dot(vS', vS)+ (1/2)*(Is)*dths^2;
+
 
 % ---------------------------------- %
 
@@ -99,21 +116,27 @@ P5 = m5*g*dot(rM5,jhat);
 Pky = 1/2*k*(dot(rI,jhat)^2-dot(r_heela,jhat)^2); %break down spring force into x and y components
 Pkx = 1/2*k*(dot(rI,ihat)^2-dot(r_heela,ihat)^2); 
 
+%SWING LEG
+Ps = ms*g*dot(rMs,jhat);
+
 % Compute entire system energy
 %T = simplify(T1 + T2 + T3 + T4 +T5); 
-T = simplify(T0 + T1 + T2 + T3 + T4 +T5); %with x movement
+T = simplify(T0 + T1 + T2 + T3 + T4 + T5 + Ts); %with x movement
 %V = P1+P2+P3+P4+P5;
-V = P1+P2+P3+P4+P5+Pky+Pkx;
+V = P1+P2+P3+P4+P5+Pky+Pkx + Ps;
 
 % Find Generalized forces
 Q_tau1 = M2Q(tau1*khat,dth1*khat);
 Q_tau2 = M2Q(tau2*khat,dth2*khat);
 %foot
 Q_tau3 = M2Q(tau3*khat,dth3*khat);
-Q = Q_tau1 + Q_tau2 + Q_tau3;
+%swing
+Q_taus = M2Q(taus*khat,dths*khat);
+
+Q = Q_tau1 + Q_tau2 + Q_tau3 + Q_taus;
 
 % Assemble the array of cartesian coordinates of the key points
-keypoints = [r0(1:2) rA(1:2) rB(1:2) rC(1:2) rD(1:2) rE(1:2) rI(1:2) rH(1:2) r_heela(1:2)];
+keypoints = [r0(1:2) rA(1:2) rB(1:2) rC(1:2) rD(1:2) rE(1:2) rI(1:2) rH(1:2) r_heela(1:2) rHs(1:2)];
 
 %% All the work is done!  Just turn the crank...
 % Derive Energy Function and Equations of Motion
@@ -141,10 +164,20 @@ matlabFunction(rI,'file',['position_heel'],'vars',{z p});
 matlabFunction(drI,'file',['velocity_heel'],'vars',{z p});
 Jheel= jacobian(rI,q);
 matlabFunction(Jheel,'file',['jacobian_heel'],'vars',{z p});
+
+%heel attachment 
+matlabFunction(r_heela,'file',['position_heel_attachment'],'vars',{z p});
+
 %ankle 
 matlabFunction(rE,'file',['position_ankle'],'vars',{z p});
 matlabFunction(drE,'file',['velocity_ankle'],'vars',{z p});
 Jankle= jacobian(rE,q);
 matlabFunction(Jankle,'file',['jacobian_ankle'],'vars',{z p});
+
+%Swing leg toe
+matlabFunction(rHs,'file',['position_toe_swing'],'vars',{z p}); 
+matlabFunction(drHs,'file',['velocity_toe_swing'],'vars',{z p});
+Jtoes = jacobian(rHs,q);
+matlabFunction(Jtoes,'file',['jacobian_toe_swing'],'vars',{z p});
 
 matlabFunction(keypoints,'file',['keypoints_' name],'vars',{z p});
--- simulate_system_leg.m
+++ simulate_system_leg.m
@@ -1,14 +1,14 @@
 function simulate_system_leg()
 
-    %% Definte fixed paramters
+     %% Definte fixed paramters
     m0 = 0.03; %MASS OF BOOM, ETC
     m1 =.0393 + .2;         m2 =.0368; 
     m3 = .00783;            m4 = .0155;
-    m5 = .00283;
+    m5 = .00283;            
     
     I1 = 25.1 * 10^-6;      I2 = 53.5 * 10^-6;
     I3 = 9.25 * 10^-6;      I4 = 22.176 * 10^-6;
-    I5 = 2.25 * 10^-6;
+    I5 = 1876 * 10^-9;      
     
     l_OA=.011;              l_OB=.042; 
     l_AC=.096;              l_DE=.091;
@@ -17,39 +17,67 @@ function simulate_system_leg()
     l_A_m3=0.0622;          l_C_m4=0.0610;
     c5=0.01;
     
+    %SWING LEG
+    ms = .05;
+    Is = 25.1 * 10^-6;
+    ls = .13;
+    l_C_s = .06;
+    
+    
     N = 18.75;
     Ir = 0.0035/N^2;
     g = 9.81;
     %g=0;
-    k=5;
+    k=1;
     
     restitution_coeff = 0.;
-    friction_coeff = 10;
+    friction_coeff = 1000;
     ground_height = 0;
     
     %% Parameter vector
-    p   = [m0 m1 m2 m3 m4 m5 I1 I2 I3 I4 I5 Ir N l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_OA l_OB l_AC l_DE l_IG l_GH l_heela g k]';        % parameters
-    
+    p   = [m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is Ir N l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_OA l_OB l_AC l_DE l_IG l_GH l_heela ls l_C_s g k]';        % parameters
+
     %% Perform Dynamic simulation
-    dt = 0.0001;
+    dt = 0.001;
     tf = 3;
     num_step = floor(tf/dt);
     tspan = linspace(0, tf, num_step); 
-    z0 = [0; .25; -pi/4; pi/2; 0; 0; 0; 0; 0; 0]; %moves with x velocity!
-    z_out = zeros(10,num_step);
+    z0 = [0; .25; -pi/4; pi/2; 0; 0; 0; 0; 0; 0; 0; 0]; %moves with x velocity!
+    z_out = zeros(12,num_step);
     z_out(:,1) = z0;
-      
+    %debugging
+    vx = zeros(1,num_step); 
+    condition = zeros(1,num_step); 
+    
     for i=1:num_step-1
         dz = dynamics(tspan(i), z_out(:,i), p);
+        z_out(:,i+1) = z_out(:,i) + dz*dt; 
         %discrete impact
-        z_out(6:10,i+1) = discrete_impact_contact(z_out(:,i+1),p, restitution_coeff, friction_coeff, ground_height);
-        z_out(1:5,i+1) = z_out(1:5,i) + z_out(6:10,i+1)*dt; %CHECK
+         [z_out(7:12,i+1), condition(i)] = discrete_impact_contact(z_out(:,i+1),p, restitution_coeff, friction_coeff, ground_height);
+        z_out(1:6,i+1) = z_out(1:6,i) + z_out(7:12,i+1)*dt; %CHECK
+        v = velocity_toe(z_out(:,i),p); 
+        
+        %debugging statements
+        vx(i) = v(1); 
+        disp(num2str(i*dt)); 
     end
 
     %% Compute Energy
     E = energy_leg(z_out,p);
     figure(1); clf
     plot(tspan,E);xlabel('Time (s)'); ylabel('Energy (J)');
+    
+    %% Check forces and vel over time
+    figure(6); clf;
+    plot(tspan, vx);
+    xlabel('time'); 
+    ylabel('VxToe'); 
+    
+    %% Check the contact condition over time
+    figure(7); clf;
+    plot(tspan, condition);
+    xlabel('time'); 
+    ylabel('Contact Condition'); 
     
     %% Compute foo, heel and ankle position over time
     rH = zeros(3,length(tspan));
@@ -86,36 +114,73 @@ function simulate_system_leg()
     animateSol(tspan,z_out,p);
 end
 
-function qdot = discrete_impact_contact(z,p, rest_coeff, fric_coeff, yC)
-    y = position_toe(z,p); %or toe?
+function [qdot, c] = discrete_impact_contact(z,p, rest_coeff, fric_coeff, yC)
+
+    %who is in contact w/ ground?
+    %TOE CONTACT
+    y = position_toe(z,p); 
     y = -y(2);
     y_dot = velocity_toe(z,p);
     Cy = y-yC;
     Cy_dot = y_dot(2);
     
-    J = jacobian_toe(z,p);
-    Mm = A_leg(z,p);
-    
-    lamY = inv(J(2,:)*inv(Mm)*J(2,:)');
-    lamX = inv(J(1,:)*inv(Mm)*J(1,:)');
-    
-    %where z(3,4) = q_dot   
-    if (Cy > 0 || Cy_dot > 0)
-        qdot = z(6:10);
-    else
+    %HEEL CONTACT
+    y2 = position_heel(z,p); 
+    y2 = -y2(2);
+    y_dot2 = velocity_heel(z,p);
+    Cy2 = y2-yC;
+    Cy_dot2 = y_dot2(2); 
+  
+        
+    if (Cy <= 0 && Cy_dot <= 0)
+        c =1; 
+        disp('toe touching');
+        
+        J = jacobian_toe(z,p);
+        Mm = A_leg(z,p);
+
+        lamY = inv(J(2,:)*inv(Mm)*J(2,:)');
+        lamX = inv(J(1,:)*inv(Mm)*J(1,:)');
+        
         %update y dir
-        F_cy = lamY*(-rest_coeff*Cy_dot-(J(2,:)*z(6:10)));
-        z(6:10) = z(6:10) + inv(Mm)*J(2,:)'*F_cy;
+        F_cy = lamY*(-rest_coeff*Cy_dot-(J(2,:)*z(7:12)));
+        z(7:12) = z(7:12) + inv(Mm)*J(2,:)'*F_cy;
         %update x dir
-        F_cx = lamX*(0-J(1,:)*z(6:10));
+        F_cx = lamX*(0-J(1,:)*z(7:12));
         %truncate if necessary
         if (F_cx > fric_coeff*F_cy)
             F_cx = fric_coeff*F_cy;
         elseif (-F_cx < -fric_coeff*F_cy)
             F_cx = -fric_coeff*F_cy;
         end
-        z(6:10) = z(6:10) + inv(Mm)*J(1,:)'*F_cx; 
-        qdot = z(6:10);
+        z(7:12) = z(7:12) + inv(Mm)*J(1,:)'*F_cx; 
+        qdot = z(7:12);
+    elseif (Cy2 <= 0 && Cy_dot2 <= 0)
+        c =2;
+        disp('heel touching');
+        J = jacobian_heel(z,p);
+        Mm = A_leg(z,p);
+
+        lamY = inv(J(2,:)*inv(Mm)*J(2,:)');
+        lamX = inv(J(1,:)*inv(Mm)*J(1,:)');
+        
+        %update y dir
+        F_cy = lamY*(-rest_coeff*Cy_dot2-(J(2,:)*z(7:12)));
+        z(7:12) = z(7:12) + inv(Mm)*J(2,:)'*F_cy;
+        %update x dir
+        F_cx = lamX*(0-J(1,:)*z(7:12));
+        %truncate if necessary
+        if (F_cx > fric_coeff*F_cy)
+            F_cx = fric_coeff*F_cy;
+        elseif (-F_cx < -fric_coeff*F_cy)
+            F_cx = -fric_coeff*F_cy;
+        end
+        z(7:12) = z(7:12) + inv(Mm)*J(1,:)'*F_cx; 
+        qdot = z(7:12);
+    else
+        c=3; 
+        disp('no contact'); 
+        qdot = z(7:12); 
     end
 end
 
@@ -135,18 +200,18 @@ function tau = control_law(t,z,p)
    rE = position_ankle(z,p); %THIS VALUE NOT HAPPY
     % b. Compute J, the jacobian of r_E
    jE = jacobian_ankle(z,p); %THIS VALUE NOT HAPPY
-   jE = jE(:, 3:end);
+   jE = jE(:, 4:end); % # OF CONTROLS?
    vF = velocity_ankle(z,p);
-   rEd = [0.05; 0.05; 0]; %TRACK SINUSOID , make point g move in an ellipse
+   rEd = [0.05; 0.05; 0 ; 0]; %TRACK SINUSOID , make point g move in an ellipse
 
-   tau = -transpose(jE)*[K_x*(rE(1)-rEd(1))+D_x*vF(1);K_y*(rE(2)-rEd(2))+D_y*vF(2); 0];  %DAMPING
+   tau = -transpose(jE)*[K_x*(rE(1)-rEd(1))+D_x*vF(1);K_y*(rE(2)-rEd(2))+D_y*vF(2); 0 ; 0];  %DAMPING
 end
 
 
 function Fc = contact_force_toe(z,p)
 
     %% Fixed parameters for contact
-    K_c = 100;
+    K_c = 2*10^4;
     D_c = 5;
     yC  = 0;
     
@@ -172,7 +237,7 @@ end
 function Fh = contact_force_heel(z,p)
 
     %% Fixed parameters for contact
-    K_c = 100;
+    K_c = 2*10^4;
     D_c = 5;
     yC  = 0;
     
@@ -195,14 +260,18 @@ function Fh = contact_force_heel(z,p)
     end 
 end
 
-function Ankle_Res = ankle_constraint(z)
+function Ankle_Res = ankle_constraint(z,p)
+    %make sure the position of the heel is always to the left of that of
+    %the ankle -- can't go past the vector of the ankle
+
     th3 = z(5);
-%     if th3 < 0
-%         th3=0;
-%     end
-%     if th3 > 178
-%         th3=178;
-%     end
+    th1 = z(3);
+    if th3 <= 0
+        th3=0.;
+    end
+    if th3 > pi/8
+        th3= pi/8;
+    end
 	Ankle_Res = th3;
 end
 
@@ -224,11 +293,11 @@ function dz = dynamics(t,z,p)
 %     dth1 = z(7);  dth2= z(8);      dth3 = z(9);
     
     % Get mass matrix
-    A = A_leg(z,p);
+    A = A_leg(z,p)
     
     % Compute Controls
     %tau = control_law(t,z,p); %tau EXPLODING WITH CONTROLS
-    tau = [0;0;0]; 
+    tau = [0;0;0;0]; 
     
 %     % Get b = Q - V(q,qd) - G(q)
 %     b = b_leg(z,tau,p); 
@@ -244,30 +313,33 @@ function dz = dynamics(t,z,p)
     QFc_h=  transpose(J_heel)*[0;Fh;0];
     QFc = QFc_t +  QFc_h; 
     
-    z(5) = ankle_constraint(z);
-    
+    z(5) = ankle_constraint(z,p);
+    %controls
     t1= z(3);
     t2 = z(4);
     td1 = -pi/4;
     td2 = pi/2;
+    ts = z(6);
+    tsd = 0;
+    
     K= 100;
     D=5;
-    tau = [K*(td1-t1)+D*-t1;K*(td2-t2)+D*-t2; 0];
-   
+    tau = [K*(td1-t1)+D*-t1;K*(td2-t2)+D*-t2; 0 ; K*(tsd-ts)+D*-ts];
+    
     % Get b = Q - V(q,qd) - G(q)
-    b = b_leg(z,tau,p);
+    b = b_leg(z,tau,p)
     
     % Compute the contact force (used for problem 2.5)
     Tauc = joint_limit_torque(z,p);
-    QTauc= [0; 0; 0; 0;0]; %column vector
+    QTauc= [0; 0; 0; 0;0;0]; %column vector
     
     % Solve for qdd.
     qdd = A\(b + QFc + QTauc);
     dz = 0*z;
     
     % Form dz
-    dz(1:5) = z(6:10);
-    dz(6:10) = qdd;
+    dz(1:6) = z(7:12);
+    dz(7:12) = qdd;
 end
 
 function animateSol(tspan, x,p)
@@ -281,6 +353,8 @@ function animateSol(tspan, x,p)
     h_GH = plot([0],[0],'LineWidth',2);
     %THIS IS OUR ANKLE SPRING
     h_ankle = plot([0],[0],'LineWidth',0.5);
+    
+    h_swing = plot([0],[0],'LineWidth',0.5);
    
     
     xlabel('x'); ylabel('y');
@@ -308,6 +382,7 @@ function animateSol(tspan, x,p)
         rI = keypoints(:,7);
         rH = keypoints(:,8);
         r_heela = keypoints(:,9);
+        r_swing = keypoints(:,10);
 
         set(h_title,'String',  sprintf('t=%.2f',t) ); % update title
         
@@ -333,6 +408,10 @@ function animateSol(tspan, x,p)
         %ANKLE
         set(h_ankle, 'XData' , [r_heela(1) rI(1)] );
         set(h_ankle, 'YData' , [r_heela(2) rI(2)] );
+        
+        %SWING LEG
+        set(h_swing, 'XData' , [r0(1) r_swing(1)] );
+        set(h_swing, 'YData' , [r0(2) r_swing(2)] );
 
         pause(.05)
     end
