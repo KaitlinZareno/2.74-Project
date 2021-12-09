22b0962c53b07c8d7afd7abd8d759f60811292c6
KaitlinZareno <kaitlinzareno@gmail.com>
2021-12-08 20:27:08 -0500

    ankle changes

--- derive_system_leg.m
+++ derive_system_leg.m
@@ -1,8 +1,8 @@
 name = 'leg';
 
 % Define variables for time, generalized coordinates + derivatives, controls, and parameters 
-syms x y t th1 th2 th3 ths xs ys dx dy dth1 dth2 dth3 dths dxs dys ddx ddy ddth1 ddth2 ddth3 ddths ddxs ddys real
-syms m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_C_s ls g k real
+syms x y t th1 th2 th3 ths dx dy dth1 dth2 dth3 dths ddx ddy ddth1 ddth2 ddth3 ddths real
+syms m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_C_s ls l_anklerest g k real
 syms l_OA l_OB l_AC l_DE l_IG l_GH l_heela real 
 syms tau1 tau2 tau3 taus Fx Fy real
 syms Ir N real
@@ -10,24 +10,31 @@ syms Ir N real
 % Group them
 q   = [x; y; th1 ; th2 ; th3; ths];      % generalized coordinates
 dq  = [dx; dy; dth1 ; dth2; dth3 ; dths];    % first time derivatives
-ddq = [ddx; ddy; ddth1;ddth2; ddth3 ; ddths];  % second time derivatives
+ddq = [ddx; ddy; ddth1; ddth2; ddth3 ; ddths];  % second time derivatives
 u   = [tau1 ; tau2; tau3; taus];     % controls
 F   = [Fx ; Fy];
 
-p   = [m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is Ir N l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_OA l_OB l_AC l_DE l_IG l_GH l_heela ls l_C_s g k]';        % parameters
+p   = [m0 m1 m2 m3 m4 m5 ms I1 I2 I3 I4 I5 Is Ir N l_O_m1 l_B_m2 l_A_m3 l_C_m4 c5 l_OA l_OB l_AC l_DE l_IG l_GH l_heela ls l_C_s g k l_anklerest]';        % parameters
 
 % Generate Vectors and Derivatives
 
 % [DEFINE HELPFUL UNIT VECTORS HERE] %
-ihat = [1; 0; 0];
-jhat = [0; 1; 0];
+ihat = [0; -1; 0];
+jhat = [1; 0; 0];
 khat = cross(ihat,jhat);
 
-th1hat = sin(th1)*ihat - cos(th1)*jhat;
-th2hat = sin(th2 + th1)*ihat - cos(th2 + th1)*jhat;
-th3hat = sin(th3 + th2 + th1)*ihat - cos(th3+ th2 + th1)*jhat;
+xhat = [1; 0; 0];
+yhat = [0; 1; 0];
 
-thshat = sin(ths)*ihat - cos(ths)*jhat; %WANT SWING LEG TO BE THE MIRROR IMAGE OF TH1
+% th1hat = sin(th1)*ihat - cos(th1)*jhat;
+% th2hat = sin(th2 + th1)*ihat - cos(th2 + th1)*jhat;
+% th3hat = sin(th3 + th2 + th1)*ihat - cos(th3+ th2 + th1)*jhat;
+
+th1hat = cos(th1)*ihat - sin(th1)*jhat;
+th2hat = cos(th2 + th1)*ihat - sin(th2 + th1)*jhat;
+th3hat = cos(th3 + th2 + th1)*ihat - sin(th3+ th2 + th1)*jhat;
+
+thshat = cos(ths)*ihat + sin(ths)*jhat; %WANT SWING LEG TO BE THE MIRROR IMAGE OF TH1
 % ---------------------------------- %
 
 ddt = @(r) jacobian(r,[q;dq])*[dq;ddq]; % a handy anonymous function for taking time derivatives
@@ -85,13 +92,19 @@ F2Q = @(F,r) simplify(jacobian(r,q)'*(F));    % force contributions to generaliz
 M2Q = @(M,w) simplify(jacobian(w,dq)'*(M));   % moment contributions to generalized forces
 
 %KINETIC ENERGY
-T0 = (1/2)*m0*dot(vR0', vR0); %INCLUDE ANOTHER ROTATIONAL INTERTIA THING FOR BOOM?
-T1 = (1/2)*m1*dot(vM1', vM1)+ (1/2)*(I1)*dth1^2; 
-T2 = (1/2)*m2*dot(vM2', vM2)+ (1/2)*(I2)*dth2^2;
-T3 = (1/2)*m3*dot(vM3', vM3)+ (1/2)*(I3)*dth2^2;
-T4 = (1/2)*m4*dot(vM4', vM4)+ (1/2)*(I4)*dth1^2;
+omega1 = dth1;
+omega2 = dth1 + dth2;
+omega3 = dth1 + dth2;
+omega4 = dth1;
+omega5 = dth1 + dth3; %CHECK!!!!
 
-T5 = (1/2)*m5*dot(vM5', vM5)+ (1/2)*(I5)*dth3^2;
+T0 = (1/2)*m0*dot(vR0', vR0); %INCLUDE ANOTHER ROTATIONAL INTERTIA THING FOR BOOM?
+T1 = (1/2)*m1*dot(vM1', vM1)+ (1/2)*(I1)*omega1^2; 
+T2 = (1/2)*m2*dot(vM2', vM2)+ (1/2)*(I2)*omega2^2;
+T3 = (1/2)*m3*dot(vM3', vM3)+ (1/2)*(I3)*omega3^2;
+T4 = (1/2)*m4*dot(vM4', vM4)+ (1/2)*(I4)*omega4^2;
+
+T5 = (1/2)*m5*dot(vM5', vM5)+ (1/2)*(I5)*omega5^2;
 
 %SWING LEG
 Ts = (1/2)*ms*dot(vS', vS)+ (1/2)*(Is)*dths^2;
@@ -99,39 +112,45 @@ Ts = (1/2)*ms*dot(vS', vS)+ (1/2)*(Is)*dths^2;
 %ROTOR INERTIA ENERGY
 T1r = (1/2)*Ir*(N*dth1)^2;
 T2r = (1/2)*Ir*(dth1 + N*dth2)^2;
-T3r = (1/2)*Ir*(dth1 + dth2 + N*dth3)^2;
-Tsr = (1/2)*Ir*(dth1 + dth2 + dth3 + N*dths)^2;
+T3r = (1/2)*Ir*(dth1 + N*dth3)^2; %CHECK
+Tsr = (1/2)*Ir*(N*dths)^2;
 
 % ---------------------------------- %
 
 %POTENTIAL ENERGY
-P1 = m1*g*dot(rM1,jhat);
-P2 = m2*g*dot(rM2,jhat);
-P3 = m3*g*dot(rM3,jhat);
-P4 = m4*g*dot(rM4,jhat);
+P1 = m1*g*dot(rM1,-ihat);
+P2 = m2*g*dot(rM2,-ihat);
+P3 = m3*g*dot(rM3,-ihat);
+P4 = m4*g*dot(rM4,-ihat);
 
 %FOOT
-P5 = m5*g*dot(rM5,jhat);
-Pky = 1/2*k*(dot(rI,jhat)^2-dot(r_heela,jhat)^2); %break down spring force into x and y components
-Pkx = 1/2*k*(dot(rI,ihat)^2-dot(r_heela,ihat)^2); 
+P5 = m5*g*dot(rM5,-ihat);
+% Pky = 1/2*k*(dot(rI,-ihat)^2-dot(r_heela,-ihat)^2); %break down spring force into x and y components
+% Pkx = 1/2*k*(dot(rI,jhat)^2-dot(r_heela,jhat)^2); 
+dist = (r_heela-rI);
+Pa = 1/2*k*((dist-l_anklerest)'*(dist-l_anklerest)); %break down spring force into x and y components
+
 
 %SWING LEG
-Ps = ms*g*dot(rMs,jhat);
+Ps = ms*g*dot(rMs,-ihat);
 
 % Compute entire system energy 
 T = simplify(T0 + T1 + T2 + T3 + T4 + T5 + Ts + T1r + T2r + T3r+ Tsr); %with x movement
-V = P1+P2+P3+P4+P5+Pky+Pkx + Ps;
+V = P1+P2+P3+P4+P5+ Pa + Ps;
 
 % Find Generalized forces
-Q_tau1 = M2Q(tau1*khat,dth1*khat);
-Q_tau2 = M2Q(tau2*khat,dth2*khat);
+Q_tau1 = M2Q(tau1*khat,omega1*khat);
+Q_tau2 = M2Q(tau2*khat,omega2*khat);
+Q_tau2R= M2Q(-tau2*khat,omega1*khat); %WHAT IS THIS
+
 %foot
-Q_tau3 = M2Q(tau3*khat,dth3*khat);
+Q_tau3 = M2Q(tau3*khat,omega3*khat);
+Q_tau3R= M2Q(-tau3*khat,omega1*khat); %WHAT IS THIS
+
 %swing
 Q_taus = M2Q(taus*khat,dths*khat);
-% Q_tau2R= M2Q(-tau2*khat,omega1*khat); %WHAT IS THIS
 
-Q = Q_tau1 + Q_tau2 + Q_tau3 + Q_taus;
+Q = Q_tau1 + Q_tau2 + Q_tau3 + Q_taus + Q_tau2R + Q_tau3R;
 
 % Assemble the array of cartesian coordinates of the key points
 keypoints = [r0(1:2) rA(1:2) rB(1:2) rC(1:2) rD(1:2) rE(1:2) rI(1:2) rH(1:2) r_heela(1:2) rHs(1:2)];
